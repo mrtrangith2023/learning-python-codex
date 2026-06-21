9589c85a37a5b508d1e6866d9ba85fb0cedd816f
mrtrangith2023 <mrtranairdrop@gmail.com>
2026-06-22 00:38:49 +0700

    Day 01 - Python Variables

---
+++ wallet.py
@@ -0,0 +1,15 @@
+# Day 1: Variables, Data types
+
+# wallet_score = 85
+
+# wallet_name = "Tran"
+
+# is_activate = True
+
+wallet_name ="My Wallet"
+
+wallet_score = 85
+
+print(wallet_name)
+
+print(wallet_score)
